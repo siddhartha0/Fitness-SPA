62e44fee82f273bdb08999035b517fb060c040ec
siddhartha0 <siddhartha.sunuwar@gmail.com>
2025-01-01 20:12:12 +0545

    font change

--- fitness-spa/src/app/layout.tsx
+++ fitness-spa/src/app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const getInter = Inter({
   subsets: ["latin"],
 });
 
@@ -24,11 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${getInter.className} antialiased`}>{children}</body>
     </html>
   );
 }
